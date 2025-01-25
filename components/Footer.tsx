b5260f9b8775c344d26ab27bab10332ac15881b6
Chloe <gtalk.with.nani@gmail.com>
2025-01-25 19:17:06 +0900

    기본 스타일 정의

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+import styles from '@/styles/Footer.module.sass';
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className="container">Footer</div>
+    </footer>
+  );
+}
---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+import styles from '@/styles/Header.module.sass';
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className="container">Header</div>
+    </header>
+  );
+}
