d38cf34e50a22e891f62d2f0e6dcda204d541fab
Chloe <gtalk.with.nani@gmail.com>
2025-01-25 20:08:11 +0900

    헤더 작업

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,15 @@
 import styles from '@/styles/Header.module.sass';
+import Anchor from './Anchor';
+import { Logo } from './Svgs';
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <div className="container">Header</div>
+      <div className={`container ${styles.container}`}>
+        <Anchor href="/">
+          <Logo />
+        </Anchor>
+      </div>
     </header>
   );
 }
