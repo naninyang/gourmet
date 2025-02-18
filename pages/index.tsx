6b728b5f44334ff5bd272c18ee0169e63b4290cf
Chloe <gtalk.with.nani@gmail.com>
2025-02-18 17:12:01 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home({
   const timestamp = Date.now();
   return (
     <main className={styles.main}>
-      <Seo pageImg={`https://gourmet.dev1stud.io/og?ts=${timestamp}`} />
+      <Seo pageImg={`https://gourmet.dev1stud.io/og.webp?ts=${timestamp}`} />
       <div className={`container ${styles.container}`}>
         {error ? (
           <div className={styles.error}>
